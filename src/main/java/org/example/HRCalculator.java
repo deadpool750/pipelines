a8c3bbeed4aecc8337ea6a4461559202f0d2feb9
dpl750 <278435@student.pwr.edu.pl>
2025-11-08 18:34:55 +0100

    completed the getWorkoutZone function

--- src/main/java/org/example/HRCalculator.java
+++ src/main/java/org/example/HRCalculator.java
@@ -9,7 +9,16 @@ public class HRCalculator {
         }
         return 220 - age;
     }
-    public static String getWorkoutZone(int age, int bpm){
-        return "?";
+    public static String getWorkoutZone(int age, int bpm) {
+        int max = calculateMaxHR(age);
+        double pct = (double) bpm / max;
+
+        if (pct < 0.5) return "Below Training Zone";
+        if (pct < 0.6) return "Very Light";
+        if (pct < 0.7) return "Light";
+        if (pct < 0.8) return "Moderate";
+        if (pct < 0.9) return "Hard";
+        return "Very Hard";
     }
+
 }
